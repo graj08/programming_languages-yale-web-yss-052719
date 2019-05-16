4415fa49740fb1829bab89a0f32bb501d8edc488
gorgeous-karma-7335 <graj08@gmail.com>
2019-05-16 23:35:38 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -6,7 +6,7 @@ def reformat_languages(languages)
   languages.each do |style,lang_and_type|
     lang_and_type.each do |lang,attributes|
     new_hash[lang] = attributes
-    if new_hash[lang][:style] != []
+    if new_hash[lang][:style] != nil
     new_hash[lang][:style] << style
   else
   new_hash[lang][:style] = Array.new
