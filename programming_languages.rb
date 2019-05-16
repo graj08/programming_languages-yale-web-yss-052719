c3d7c3a51241ff9469cade2be62b80aed55543e6
gorgeous-karma-7335 <graj08@gmail.com>
2019-05-16 23:15:33 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,5 +1,9 @@
 def reformat_languages(languages)
   # your code here
   new_hash = Hash.new
-  
+  languages.each do |style,lang_and_type|
+    new_hash[lang_and_type] = lang_and_type
+    new_hash[lang_and_type][:style] = style
+  end
+  new_hash
 end
