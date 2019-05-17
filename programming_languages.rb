0c09ce7aa4473b8dcff76152c43104312050563c
gorgeous-karma-7335 <graj08@gmail.com>
2019-05-17 00:05:33 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,11 +3,20 @@ require 'pry'
 def reformat_languages(languages)
   # your code here
   new_hash = Hash.new
+
   languages.each do |style,lang_and_type|
     lang_and_type.each do |lang,attributes|
     new_hash[lang] = attributes
   end
 end
+new_hash.each do |lang,attributes|
+  attributes[:style] = Array.new
+end
+new_hash.each do |lang,attributes|
+  if languages[:oo].include?(lang)
+    attributes[:style] << :oo
+  if languages[:functional].include?(lang)
+    attributes[:style] << :functional
 binding.pry
 new_hash
 end
