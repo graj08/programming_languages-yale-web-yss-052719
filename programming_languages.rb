186dd844065efd21c01b1b7de3c7366d55137988
gorgeous-karma-7335 <graj08@gmail.com>
2019-05-17 00:07:27 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,24 @@
+require 'pry'
+
 def reformat_languages(languages)
   # your code here
+  new_hash = Hash.new
+
+  languages.each do |style,lang_and_type|
+    lang_and_type.each do |lang,attributes|
+    new_hash[lang] = attributes
+  end
+end
+new_hash.each do |lang,attributes|
+  attributes[:style] = Array.new
+end
+new_hash.each do |lang,attributes|
+  if languages[:oo].include?(lang)
+    attributes[:style] << :oo
+  end
+  if languages[:functional].include?(lang)
+    attributes[:style] << :functional
+  end
+end
+new_hash
 end
